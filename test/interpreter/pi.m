e3a51227b0e8c22dfec3afaaee22ea4a4f7421e1
krotka <oliwiapolonia@gmail.com>
2026-01-03 01:08:23 +0100

    remove debug stuff

--- test/interpreter/pi.m
+++ test/interpreter/pi.m
@@ -1,9 +1,7 @@
 pi = 0.0;
 n = 1;
 for i = 1:100000 {
-    # TODO bad parsing of arithmetic
     pi += 4.0 / n - 4.0 / (n + 2);
-    # print pi, test, 4.0 / n, 4.0 / (n + 2);
     n += 4;
 }
 print pi;
